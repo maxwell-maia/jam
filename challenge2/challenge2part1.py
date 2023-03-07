e7a1ab98f1691fed27d53af4cf128c4555844c88
Maxwell Maia <115161307+maxwell-maia@users.noreply.github.com>
2023-03-07 14:05:54 +0000

    Update challenge2part1.py

--- challenge2/challenge2part1.py
+++ challenge2/challenge2part1.py
@@ -8,6 +8,7 @@ all_employees = [
         'BAILEY REED','HANẞ HOWARD','RAMOS KIM','CÖX WARD','RICHARDSON WATSON','BROOKS CHAVEZ',
         'WOOD JAMES','BENNETT GRAY','MENDOZA RUIZ','HUGHES PRICE','ALVAREZ çASTILLO'
     ]
+    #46 names in array
 
 def get_all_employees_input(message):
     """ Ignore this function you don't need to understand or change it """
@@ -21,7 +22,18 @@ def get_all_employees_input(message):
 def problem2_1(message):
     all_employees = get_all_employees_input(message)
     beginsWithLetterCCount = 0
+    
+    #loop through the array: all_employees
+    for name in all_employees:
+        #make the whole name lower case
+        name = name.lower()
+        #if the name starts with "c" then: beginsWithLetterCCount++
+        if name.startswith("c"):
+            print(name)
+            beginsWithLetterCCount += 1
 
+    
+    
 
     return str(beginsWithLetterCCount)
 
@@ -30,6 +42,18 @@ def problem2_2(message):
     all_employees = get_all_employees_input(message)
     peopleWithLongerThan8CharacterLastnamesCount = 0
 
+    last_name = ""
+
+    #loop through the array: all_employees
+    for name in all_employees:
+        #get the last name
+        last_name_list = name.split()
+        last_name = last_name_list[1]
+
+        #if the last_name has more than 8 characters then: peopleWithLongerThan8CharacterLastnamesCount++
+        if len(last_name) > 8:
+            print(name)
+            peopleWithLongerThan8CharacterLastnamesCount += 1
 
     return str(peopleWithLongerThan8CharacterLastnamesCount)
 
@@ -38,5 +62,16 @@ def problem2_3(message):
     all_employees = get_all_employees_input(message)
     employeesWhoWillGetBonusesCount = 0
 
-
+    #loop through the array: all_employees
+    for name in all_employees:
+        #if the name has "ẞ, ö, æ, or ç" then: employeesWhoWillGetBonusesCount++
+        #name.contains("ẞ") or name.contains("ö") or name.contains("æ") or name.contains("ç")
+        if "ẞ" in name or "ö" in name or "æ" in name or "ç" in name:
+            print(name)
+            employeesWhoWillGetBonusesCount += 1
+    
     return str(employeesWhoWillGetBonusesCount)
+
+print(problem2_1(all_employees))
+print(problem2_2(all_employees))
+print(problem2_3(all_employees))
